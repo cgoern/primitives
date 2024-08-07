171931a99d5c707522e55a3a4f9e3f636c831ac9
cgoern <goern@fastmail.net>
2024-08-07 18:15:49 +0200

    Round typography values; Introduce leading trim

--- src/text.js
+++ src/text.js
@@ -6,8 +6,8 @@ styles.replaceSync(`
 		display: block;
 		font-family: var(--ui-text-font-family, var(--ui-font-family-primary, sans-serif));
 		letter-spacing: var(--ui-text-letter-spacing, var(--ui-letter-spacing-primary, normal));
-		font-size: calc(1rem * pow(var(--ui-font-size-ratio, 1.2), var(--ui-text-font-size-exponent, 0)));
-		line-height: calc(((16px / 1.6) + 1em) * var(--ui-line-height-factor, 1));
+		font-size: round(nearest, calc(1rem * pow(var(--ui-font-size-ratio, 1.2), var(--ui-text-font-size-exponent, 0))), 1px);
+		line-height: round(nearest, calc(((16px / 1.6) + 1em) * var(--ui-line-height-factor, 1)), 1px);
 		font-feature-settings: var(--ui-text-font-feature-settings, var(--ui-font-feature-settings-primary, 'liga' 1));
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
@@ -31,44 +31,37 @@ styles.replaceSync(`
         --ui-text-font-feature-settings: var(--ui-font-feature-settings-tertiary, normal);
 	}
 
-	:host([level='-2']) {
-	    --ui-text-font-size-exponent: -2;
+	:host([level='-2']) { --ui-text-font-size-exponent: -2; }
+	:host([level='-1']) { --ui-text-font-size-exponent: -1; }
+	:host([level='0']) { --ui-text-font-size-exponent: 0; }
+	:host([level='+1']) { --ui-text-font-size-exponent: 1; }
+	:host([level='+2']) { --ui-text-font-size-exponent: 2; }
+	:host([level='+3']) { --ui-text-font-size-exponent: 3; }
+	:host([level='+4']) { --ui-text-font-size-exponent: 4; }
+	:host([level='+5']) { --ui-text-font-size-exponent: 5; }
+	:host([level='+6']) { --ui-text-font-size-exponent: 6; }
+
+	.content {
+	    padding: 1px 0;
 	}
 
-	:host([level='-1']) {
-	    --ui-text-font-size-exponent: -1;
+	.content::before,
+	.content::after {
+		content: '';
+		display: block;
+		height: 0;
 	}
 
-	:host([level='0']) {
-	    --ui-text-font-size-exponent: 0;
+	.content::before {
+		margin-top: round(nearest, calc(((1lh - 1cap) / -2) - 1px), 1px);
 	}
 
-	:host([level='+1']) {
-	    --ui-text-font-size-exponent: 1;
-	}
-
-	:host([level='+2']) {
-	    --ui-text-font-size-exponent: 2;
-	}
-
-	:host([level='+3']) {
-	    --ui-text-font-size-exponent: 3;
-	}
-
-	:host([level='+4']) {
-	    --ui-text-font-size-exponent: 4;
-	}
-
-	:host([level='+5']) {
-	    --ui-text-font-size-exponent: 5;
-	}
-
-	:host([level='+6']) {
-	    --ui-text-font-size-exponent: 6;
+	.content::after {
+		margin-bottom: round(nearest, calc(((1lh - 1cap) / -2) - 1px), 1px);
 	}`)
 
 template.innerHTML = `
-	<div>
+	<div class="content">
 		<slot>Content</slot>
 	</div>`
 
