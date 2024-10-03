729fb3b488a434cf163766d67f88144db9772416
cgoern <goern@fastmail.net>
2024-10-03 14:06:58 +0200

    Enable underlining; Rename custom properties

--- src/text.js
+++ src/text.js
@@ -6,178 +6,147 @@ const html = String.raw
 styles.replaceSync(css`
   :host {
     font-family: var(
-      --ui-text-font-family,
-      var(--ui-font-family-primary, sans-serif)
+      --UIText-FontFamily,
+      var(--UI-FontFamily-Primary, sans-serif)
     );
 
     font-size: round(
       nearest,
       calc(
         1rem *
-          pow(
-            var(--ui-font-size-ratio, 1.2),
-            var(--ui-text-font-size-exponent, 0)
-          )
+          pow(var(--UI-FontSizeRatio, 1.2), var(--UIText-FontSizeExponent, 0))
       ),
       1px
     );
 
     letter-spacing: var(
-      --ui-text-letter-spacing,
-      var(--ui-letter-spacing-primary, normal)
+      --UIText-LetterSpacing,
+      var(--UI-LetterSpacing-Primary, normal)
     );
 
     line-height: round(
       nearest,
-      calc(((16px / 1.6) + 1em) * var(--ui-line-height-factor, 1)),
+      calc(((16px / 1.6) + 1em) * var(--UI-LineHeightFactor, 1)),
       2px
     );
 
     font-feature-settings: var(
-      --ui-text-font-feature-settings,
-      var(--ui-font-feature-settings-primary, 'liga' 1)
+      --UIText-FontFeatureSettings,
+      var(--UI-FontFeatureSettings-Primary, 'liga' 1)
     );
 
-    text-decoration-line: var(
-      --ui-text-text-decoration-line,
-      var(--ui-text-decoration-line, none)
-    );
-
-    text-decoration-color: var(
-      --ui-text-text-decoration-color,
-      var(--ui-text-decoration-color, currentColor)
-    );
-
-    text-decoration-style: var(
-      --ui-text-text-decoration-style,
-      var(--ui-text-decoration-style, solid)
-    );
-
-    text-decoration-thickness: var(
-      --ui-text-text-decoration-thickness,
-      var(--ui-text-decoration-thickness, auto)
-    );
-
-    text-underline-offset: var(
-      --ui-text-text-underline-offset,
-      var(--ui-text-underline-offset, auto)
-    );
-
-    font-weight: var(--ui-text-font-weight, var(--ui-font-weight-400, 400));
+    font-weight: var(--UIText-FontWeight, var(--UI-FontWeight-400, 400));
     font-optical-sizing: auto;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
   :host([family='primary']) {
-    --ui-text-font-family: var(--ui-font-family-primary, sans-serif);
-    --ui-text-letter-spacing: var(--ui-letter-spacing-primary, normal);
-    --ui-text-font-feature-settings: var(
-      --ui-font-feature-settings-primary,
-      normal
-    );
+    --UIText-FontFamily: var(--UI-FontFamily-Primary, sans-serif);
+    --UIText-LetterSpacing: var(--UI-LetterSpacing-Primary, normal);
+    --UIText-FontFeatureSettings: var(--UI-FontFeatureSettings-Primary, normal);
   }
 
   :host([family='secondary']) {
-    --ui-text-font-family: var(--ui-font-family-secondary, serif);
-    --ui-text-letter-spacing: var(--ui-letter-spacing-serif, normal);
-    --ui-text-font-feature-settings: var(
-      --ui-font-feature-settings-secondary,
+    --UIText-FontFamily: var(--UI-FontFamily-Secondary, serif);
+    --UIText-LetterSpacing: var(--UI-LetterSpacing-Secondary, normal);
+    --UIText-FontFeatureSettings: var(
+      --UI-FontFeatureSettings-Secondary,
       normal
     );
   }
 
   :host([family='tertiary']) {
-    --ui-text-font-family: var(--ui-font-family-tertiary, monospace);
-    --ui-text-letter-spacing: var(--ui-letter-spacing-tertiary, normal);
-    --ui-text-font-feature-settings: var(
-      --ui-font-feature-settings-tertiary,
+    --UIText-FontFamily: var(--UI-FontFamily-Tertiary, monospace);
+    --UIText-LetterSpacing: var(--UI-LetterSpacing-Tertiary, normal);
+    --UIText-FontFeatureSettings: var(
+      --UI-FontFeatureSettings-Tertiary,
       normal
     );
   }
 
   :host([level='-2']) {
-    --ui-text-font-size-exponent: -2;
+    --UIText-FontSizeExponent: -2;
   }
 
   :host([level='-1']) {
-    --ui-text-font-size-exponent: -1;
+    --UIText-FontSizeExponent: -1;
   }
 
   :host([level='0']) {
-    --ui-text-font-size-exponent: 0;
+    --UIText-FontSizeExponent: 0;
   }
 
   :host([level='+1']) {
-    --ui-text-font-size-exponent: 1;
+    --UIText-FontSizeExponent: 1;
   }
 
   :host([level='+2']) {
-    --ui-text-font-size-exponent: 2;
+    --UIText-FontSizeExponent: 2;
   }
 
   :host([level='+3']) {
-    --ui-text-font-size-exponent: 3;
+    --UIText-FontSizeExponent: 3;
   }
 
   :host([level='+4']) {
-    --ui-text-font-size-exponent: 4;
+    --UIText-FontSizeExponent: 4;
   }
 
   :host([level='+5']) {
-    --ui-text-font-size-exponent: 5;
+    --UIText-FontSizeExponent: 5;
   }
 
   :host([level='+6']) {
-    --ui-text-font-size-exponent: 6;
+    --UIText-FontSizeExponent: 6;
   }
 
   :host([level='+7']) {
-    --ui-text-font-size-exponent: 7;
+    --UIText-FontSizeExponent: 7;
   }
 
   :host([level='+8']) {
-    --ui-text-font-size-exponent: 8;
+    --UIText-FontSizeExponent: 8;
   }
 
   :host([weight='100']) {
-    --ui-text-font-weight: var(--ui-font-weight-100, 100);
+    --UIText-FontWeight: var(--UI-FontWeight-100, 100);
   }
 
   :host([weight='200']) {
-    --ui-text-font-weight: var(--ui-font-weight-200, 200);
+    --UIText-FontWeight: var(--UI-FontWeight-200, 200);
   }
 
   :host([weight='300']) {
-    --ui-text-font-weight: var(--ui-font-weight-300, 300);
+    --UIText-FontWeight: var(--UI-FontWeight-300, 300);
   }
 
   :host([weight='400']) {
-    --ui-text-font-weight: var(--ui-font-weight-400, 400);
+    --UIText-FontWeight: var(--UI-FontWeight-400, 400);
   }
 
   :host([weight='500']) {
-    --ui-text-font-weight: var(--ui-font-weight-500, 500);
+    --UIText-FontWeight: var(--UI-FontWeight-500, 500);
   }
 
   :host([weight='600']) {
-    --ui-text-font-weight: var(--ui-font-weight-600, 600);
+    --UIText-FontWeight: var(--UI-FontWeight-600, 600);
   }
 
   :host([weight='700']) {
-    --ui-text-font-weight: var(--ui-font-weight-700, 700);
+    --UIText-FontWeight: var(--UI-FontWeight-700, 700);
   }
 
   :host([weight='800']) {
-    --ui-text-font-weight: var(--ui-font-weight-800, 800);
+    --UIText-FontWeight: var(--UI-FontWeight-800, 800);
   }
 
   :host([weight='900']) {
-    --ui-text-font-weight: var(--ui-font-weight-900, 900);
+    --UIText-FontWeight: var(--UI-FontWeight-900, 900);
   }
 
   :host([underline]) {
-    --ui-text-text-decoration-line: var(--ui-text-decoration-line, underline);
+    --UIText-Inner-TextDecorationLine: underline;
   }
 
   .content {
@@ -198,10 +167,36 @@ styles.replaceSync(css`
   .content::after {
     margin-bottom: calc(((1lh - round(nearest, 1cap, 2px)) / -2) - 1px);
   }
+
+  .inner {
+    text-decoration-color: var(
+      --UIText-Inner-TextDecorationColor,
+      var(--UI-TextDecorationColor, currentColor)
+    );
+
+    text-decoration-style: var(
+      --UIText-Inner-TextDecorationStyle,
+      var(--UI-TextDecorationStyle, solid)
+    );
+
+    text-decoration-thickness: var(
+      --UIText-Inner-TextDecorationThickness,
+      var(--UI-TextDecorationThickness, auto)
+    );
+
+    text-underline-offset: var(
+      --UIText-Inner-TextUnderlineOffset,
+      var(--UI-TextUnderlineOffset, auto)
+    );
+
+    text-decoration-line: var(--UIText-Inner-TextDecorationLine, none);
+  }
 `)
 
-template.innerHTML = html` <div class="content">
-  <slot>Content</slot>
+template.innerHTML = html`<div class="content">
+  <span class="inner">
+    <slot>Text</slot>
+  </span>
 </div>`
 
 class UIText extends HTMLElement {
